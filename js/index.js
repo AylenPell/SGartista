44ab201bbb7dc7fae72e8b60fa216cc5a7b4856e
Aylen Pell <aylen.pell@gmail.com>
2022-09-15 21:57:44 -0300

    agregando diseño, cards desde js , probandointeracción con el formulario de contacto

--- js/index.js
+++ js/index.js
@@ -1,44 +1,81 @@
 function init(){
 
+// CONSTRUCCION DE LA GALERIA DESDE JS PARA QUE FUNCIONE COMO CARDS DE UN CARRITO
+let contGallery = document.getElementById("contGallery");
 let listaCuadros = [
-    {id: 1, titulo: "Maria de Bs As", medidas: "1.40 x 1m", tecnica: "acrílico sobre tela", precio: 14000, cantidad: 1},
-    {id: 2, titulo: "El sueño de un niño", medidas: "50 x 30cm", tecnica: "aquarela sobre papel", precio: 5000, cantidad: 1},
-    {id: 3, titulo: "Poesía", medidas: "30 x 40cm" , tecnica: "tinta y aquarela sobre papel", precio: 4500, cantidad: 1},
-    {id: 4, titulo: "Extrañas", medidas: "30 x 40cm", tecnica: "tinta y carbonilla sobre papel", precio: 4500, cantidad: 1},
-    {id: 5, titulo: "Volar", medidas: "50 x 40cm", tecnica: "aquarela y grafito sobre papel", precio: 5500, cantidad: 1},
-    {id: 6, titulo: "La ternura", medidas: "25 x 23cm", tecnica: "óleo sobre papel", precio: 4300, cantidad: 1},
-    {id: 7, titulo: "Butih", medidas: "35 x 25cm", tecnica: "acrílico y tinta sobre papel", precio: 4700, cantidad: 1},
-    {id: 8, titulo: "Metamorfosis", medidas: "24 x 32cm", tecnica: "acrílico y tinta sobre papel", precio: 4100, cantidad: 1},
-    {id: 9, titulo: "Árbol de la vida", medidas: "1 x 1m", tecnica: "óleo sobre tels", precio: 11000, cantidad: 1}
+    {id: 1, titulo: "María de Bs As", medidas: "1.40 x 1m", tecnica: "acrílico sobre tela", precio: 14000, imagen: `../img/maria.jpg`, cantidad: 1},
+    {id: 2, titulo: "El sueño de un niño", medidas: "50 x 30cm", tecnica: "aquarela sobre papel", precio: 5000, imagen: `../img/sueno.jpg`, cantidad: 1},
+    {id: 3, titulo: "La Poesía", medidas: "30 x 40cm" , tecnica: "tinta y aquarela sobre papel", precio: 4500, imagen: `../img/laPoesia.jpg`, cantidad: 1},
+    {id: 4, titulo: "La Doma", medidas: "40 x 50cm", tecnica: "lápiz y carbonilla sobre papel, técnica mixta", precio: 6000, imagen: `../img/laDoma.jpg`, cantidad: 1},
+    {id: 5, titulo: "Volar", medidas: "50 x 40cm", tecnica: "aquarela y grafito sobre papel", precio: 5500, imagen: `../img/volar.jpg`, cantidad: 1},
+    {id: 6, titulo: "La Ternura", medidas: "25 x 23cm", tecnica: "óleo sobre papel", precio: 4300, imagen: `../img/laTernura.jpg`, cantidad: 1},
+    {id: 7, titulo: "Butih", medidas: "35 x 25cm", tecnica: "acrílico y tinta sobre papel", precio: 4700, imagen: `../img/butih.jpg`, cantidad: 1},
+    {id: 8, titulo: "Metamorfosis", medidas: "24 x 32cm", tecnica: "acrílico y tinta sobre papel", precio: 4100, imagen: `../img/metamorfosis.jpg`, cantidad: 1},
+    {id: 9, titulo: "Árbol de la vida", medidas: "1 x 1m", tecnica: "óleo sobre tela", precio: 11000, imagen: `../img/arbolVida.jpg`, cantidad: 1},
+    {id: 10, titulo: "Madre tierra", medidas: "1 x 0.8m", tecnica: "óleo sobre tela, técnica mixta", precio: 11000, imagen: `../img/madreTierra.jpg`, cantidad: 1}
 ]
 
-let nombre = prompt("Hola!🙂​ Bienvenido/a a la galería de arte de Silvia Gentile... Cómo es tu nombre?");
-while (!nombre){
-    nombre = prompt("Oops!😓​ Vuelve a ingresar tu nombre!");
+for(const cuadro of listaCuadros){
+    let div = document.createElement("div");
+    div.innerHTML = `
+                    <div class="divCard"> 
+                        <div class="divFcenter">
+                            <img class="imgCard" src=${cuadro.imagen} alt=${cuadro.titulo}> 
+                        </div> 
+                        <div>   
+                            <h2 class="h2Ngo"> ${cuadro.titulo} </h2>
+                            <p class="pNgo"> <span class="spBld">Técnica:</span> ${cuadro.tecnica} </p>
+                            <p class="pNgo"> <span class="spBld">Medidas:</span> ${cuadro.medidas} </p>
+                            <p class="pNgo"> <span class="spBld">Precio:</span> $${cuadro.precio} </p>
+                        </div>
+                        <div class="divFcenter">
+                            <button type="button" class="boton">Lo quiero!</button>
+                        </div>
+                    </div>
+    `;
+    contGallery.append(div);
 }
 
-let iniciar = prompt(`Un gusto en saludarte ${nombre}! Gracias por pasar a conocer mi obra 🥰​ Tenés interés en adquirir alguna pieza? SI/NO`);
+// INTERACCION CON EL FORMULARIO DE CONTACTO
+function recogeDatos (evento) {
+    evento.preventDefault ();
 
-if (iniciar.toUpperCase() === "SI"){
+    let nombre = document.querySelector("#nombre").value;
+    let cel = document.querySelector("#cel").value;
+    let mensaje = document.querySelector("#mensaje");
+    let texto = `Hola ${nombre}, 😊 tu mensaje ha sido enviado!
+                Te estare contactando al celular ${cel} para responderte.`;
+    
+    mensaje.textContent = texto;
+}
+function eliminar () {
+    mensaje.remove();       
+}
 
+let miForm = document.querySelector("#formContacto");
+let botonB = document.querySelector("#btnB");
+miForm.addEventListener("submit", recogeDatos);
+botonB.addEventListener("click", eliminar);
+
+
+
+/*     BORRE LOS PROMPTS Y ALERTS, FALTA ARMAR LA PARTE DEL CARRITO
 let carrito = [];
 
 let menuCuadros = listaCuadros.map (cuadro => {
     return `ID: ${cuadro.id} | Titulo: ${cuadro.titulo} | Medidas: ${cuadro.medidas} | Tecnica: ${cuadro.tecnica} | Precio $${cuadro.precio} \n`
 });
 
+let boton = document.getElementsByClassName("boton");
+boton.addEventListener("click", comprar);  
+
 let idElegido = "";
 const comprar = function (mensaje){
-    idElegido = prompt (`${mensaje} \n ${menuCuadros}`).toUpperCase(); 
-    let cuadroElegido = listaCuadros.find (cuadro => cuadro.id === parseInt(idElegido));
+    idElegido = //como identifica que boton apreto???
+    let cuadroElegido = listaCuadros.find (cuadro => cuadro.id === idElegido);
     if (cuadroElegido) { 
         carrito.push(cuadroElegido);
-    }
-    else if (idElegido === "X"){
-        return
-    }
-    else {
-        alert ("Oops!😓​ El ID no existe");
+        alert("Se agrego la obra a tu carrito")
     }
 }
 
@@ -52,6 +89,7 @@ console.log(carrito);
 let total = 0;
 let seleccion = carrito.map (cuadro => {
     total = total + cuadro.precio;
+    
     return `Titulo: ${cuadro.titulo} | Precio $${cuadro.precio} \n`
 });
 
@@ -61,14 +99,7 @@ El total a abonar es de $${total}
 
 Gracias por comprar arte ❤️`);
 
-}
-if (iniciar.toUpperCase() === "NO"){
-alert("Ya habra oportunidad!😊​ Te espero cuando quieras");
-}
-
-if (iniciar === ""){
-iniciar = prompt("Oops!😓 Parece que no ingresaste nada... Tenés interés en adquirir alguna pieza? SI/NO");
-}
+ */
 
 
 }
